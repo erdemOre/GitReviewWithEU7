8d8ff2e38e1fecc84a25ade92b150e2dce4f4829
Erdem <benyunusum@gmail.com>
2021-12-19 22:54:12 +0300

    things happening

---
+++ src/test/java/gitHubMergeFeature/Test01.java
@@ -0,0 +1,10 @@
+package gitHubMergeFeature;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("I made these changes locally, but I will mege them at the GitHub");
+
+    }
+}
